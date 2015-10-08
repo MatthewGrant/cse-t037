7d9d39e6cdb9f9feec61dd5044b39c08004ec093
Matthew Grant <mggrant@buffalo.edu>
2015-10-08 10:20:35 -0400

    added more classes

---
+++ Carcassonne/src/code/TileFeature.java
@@ -0,0 +1,23 @@
+package code;
+
+/**
+ *  feature that resides on a tile.
+ *
+ */
+public abstract class TileFeature {
+	protected final Feature section1;
+	protected final Feature section2;
+	protected final Feature section3;
+	
+	/**
+	   * constructor 
+	   * @param feature what feature is contained
+	   */
+	public TileFeature(Feature feature1, Feature feature2,Feature feature3) {
+	    this.section1 = feature1;
+	    this.section2 = feature2;
+	    this.section3 = feature3;
+	}
+	
+	
+}
