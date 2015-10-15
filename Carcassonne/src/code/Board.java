7e0ab6965af7875719686a0f8dd3e9eff4e09d53
DEANNA <DEANNA@DEANNA-PC>
2015-10-15 17:46:22 -0400

    not dont with most things but here is some code will change shortly

--- Carcassonne/src/code/Board.java
+++ Carcassonne/src/code/Board.java
@@ -8,6 +8,11 @@ import java.awt.Point;
 public class Board {
 	private HashMap<Point, BaseTile> _board;
 	private Deck _deck;
+	//these are the bounds for my board
+	private int _upper = 1;
+	private int _lower = -1;
+	private int _right = 1;
+	private int _left = -1;
 	
 	/**
 	 * creating a board that is inf and putting the first tile on the board 
@@ -66,9 +71,10 @@ public class Board {
 			if(check1){
 				
 				//cheek to see if the side match up
+				//this might be wrong
 				if(adjacentTiles[0] != null){
 					
-					if(adjacentTiles[0].getBottom().equals(basetile.getTop())){
+					if(adjacentTiles[0].getBottom()==(basetile.getTop())){
 						end = true;
 						
 					}
@@ -78,7 +84,7 @@ public class Board {
 				}
 				if(adjacentTiles[1] != null){
 					
-					if(adjacentTiles[1].getLeft().equals(basetile.getRight())){
+					if(adjacentTiles[1].getLeft()==(basetile.getRight())){//changed from .euqles to ==
 						end = true;
 						
 					}
@@ -88,7 +94,7 @@ public class Board {
 				}
 				if(adjacentTiles[2] != null){
 					
-					if(adjacentTiles[2].getTop().equals(basetile.getBottom())){
+					if(adjacentTiles[2].getTop()==(basetile.getBottom())){
 						end = true;
 						
 					}
@@ -98,7 +104,7 @@ public class Board {
 				}
 				if(adjacentTiles[3] != null){
 					
-					if(adjacentTiles[3].getRight().equals(basetile.getLeft())){
+					if(adjacentTiles[3].getRight()==(basetile.getLeft())){
 						end = true;
 						
 					}
@@ -114,13 +120,32 @@ public class Board {
 	}
 	/**
 	 * if your tiles pass the methods above then place the tile
+	 * this also check to see if a tile is place at the egde of the baord and to see if the board need to be increased
 	 * @param point place wiar the tile can be placed at 
 	 * @param basetile the tile 
 	 * @return true if it was placed and false if it was not
 	 */
+	
 	public boolean place (Point point, BaseTile basetile){
+		
 		if (canBePlaced(point,basetile)){
+			//check to see if the tile is placed at the boarded
 			_board.put(point, basetile);
+			if(_upper < point.y+1 ){
+				_upper = point.y+1;
+			}
+			if(_lower > point.y-1 ){
+				_lower = point.y-1;
+			}
+			if(_left < point.x-1 ){
+				_left = point.x-1;
+			}
+			if(_right < point.x+1 ){
+				_right = point.x+1;
+			}
+			//if (View =null){
+				
+			//}
 			return true;
 		}
 		return false;
@@ -129,11 +154,40 @@ public class Board {
 	
 	 
 	/**
-	 * this is to drawa tile from the deck
+	 * this is to draw a tile from the deck
 	 * @return the tile that you have just draw
 	 */
 	public BaseTile draw(){
 		return _deck.getNextTile();
+	}
+	
+	//the geter for the boarned of the board
+	/**
+	 * @return the upper baornded
+	 */
+	public int get_upper() {
+		return _upper;
+	}
+
+	/**
+	 * @return the lower boarned
+	 */
+	public int get_lower() {
+		return _lower;
+	}
+
+	/**
+	 * @return the right boarned
+	 */
+	public int get_right() {
+		return _right;
+	}
+
+	/**
+	 * @return the left boarned 
+	 */
+	public int get_left() {
+		return _left;
 	}
 	
 	
