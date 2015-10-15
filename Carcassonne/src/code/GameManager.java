3c38eb26316ca694e4da575556b834b8b36525c8
Dan Malsch <Dan@dhcp020-205-015.wireless.buffalo.edu>
2015-10-15 14:45:06 -0400

    New gamemanager class

---
+++ Carcassonne/src/code/GameManager.java
@@ -0,0 +1,47 @@
+
+package code;
+
+import java.util.Scanner;
+import code.Deck;
+public class GameManager {
+	
+
+
+
+public static void players(String[] args){
+Scanner scanner= new Scanner(System.in);	
+System.out.print("Enter the ammount of players");
+//ask for the amount of players 
+int players= scanner.nextInt();
+//players input as an Int
+if(players < 2){
+	System.out.print("Sorry, not enough players to play");
+	scanner.next();
+}
+
+if(players > 5){
+	System.out.print("Sorry, too many players to play");
+	scanner.next();
+}
+if(players > 2 && players < 5){
+	System.out.print("Alright, lets Start! Now who goes first");
+	scanner.next();
+}
+}
+
+
+
+public static void main(String[] args){
+Scanner scanner= new Scanner(System.in);
+System.out.print("Enter your name:");
+//ask for their name
+String username= scanner.next();
+//name input as string
+System.out.print("Enter your age:");
+//ask for age (play oldest to youngest)
+int age= scanner.nextInt();
+//age input as int
+System.out.println(String.format("%s,  your age is %d",username,age));
+scanner.next();
+}
+}
