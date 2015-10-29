7868c1755bc15ccd9704340c30b3672e67e44a9d
DEANNA <DEANNA@DEANNA-PC>
2015-10-29 11:50:00 -0400

    added place method to the meeples class still might need more work on it

--- Carcassonne/src/code/Meeple.java
+++ Carcassonne/src/code/Meeple.java
@@ -1,12 +1,23 @@
 package code;
 
+import java.awt.Point;
 
 public class Meeple {
 	private final Player player;
+	private Point _point;//the point on the tile at which the meeple will be placed
 
 	
 	public Meeple(Player player){
 		this.player = player;
+	}
+	
+	/**
+	 * so we can place meeples on the tiles
+	 * @param p the point on the tile that we can place
+	 */
+	public void place(Point p) 
+	{
+		_point=p;// point on the tile
 	}
 	
 	@Override
